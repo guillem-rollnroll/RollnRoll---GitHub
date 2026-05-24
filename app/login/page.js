9654796758d999911c2fd3eda896e89fef842780
guillem-rollnroll <guillem@rollnroll.com>
2026-05-24 23:23:44 +0200

    Reporte Ambassadors con login Google

---
+++ app/login/page.js
@@ -0,0 +1,19 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const session = await getServerSession();
+  if (!session) redirect("/login");
+
+  return (
+    <iframe
+      src="/index.html"
+      style={{
+        width: "100vw",
+        height: "100vh",
+        border: "none",
+        display: "block"
+      }}
+    />
+  );
+}
