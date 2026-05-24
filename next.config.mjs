46f12d86d35eb35fd1251717eeb1c98d9fa186f8
guillem-rollnroll <guillem@rollnroll.com>
2026-05-25 00:01:08 +0200

    Fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['next-auth'],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
