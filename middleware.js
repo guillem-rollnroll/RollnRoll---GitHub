77684373b9256f52f31b57563fe738a2de2dfd53
guillem-rollnroll <guillem@rollnroll.com>
2026-05-24 23:51:21 +0200

    Fix middleware v2

--- middleware.js
+++ middleware.js
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt";
 
-export async function middleware(req) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+export function middleware(req) {
+  const token = req.cookies.get("next-auth.session-token") || 
+                req.cookies.get("__Secure-next-auth.session-token");
   
   if (!token) {
     return NextResponse.redirect(new URL("/login", req.url));
